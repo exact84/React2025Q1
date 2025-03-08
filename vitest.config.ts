08d608b4304f53626cfe96ae9a5ca1953d6f9418
Alexey Trukhlyayev <exact84@gmail.com>
2025-03-09 02:37:56 +0500

    fix: several tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@context': path.resolve(__dirname, 'src/context'),
       '@store': path.resolve(__dirname, 'src/store'),
